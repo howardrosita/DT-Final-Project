c0ce28d666fffc001f6f1a1047d18d33787b63cd
Hakdooooooooog <hicap.darenzjasper@gmail.com>
2025-05-28 11:48:54 +0800

    refactor: - Enhance event service type definitions - Add react fragment to insert oother child for testing.

--- app/my-app/src/App.tsx
+++ app/my-app/src/App.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { HeroMain } from "./components/NavbarHero/HeroMain";
+import AboutEvent from "./components/AboutEvents";
 
 function App() {
-  return <HeroMain />;
+  return (
+    <>
+      <HeroMain />
+      {/* <AboutEvent /> */}
+    </>
+  );
 }
 
 export default App;
