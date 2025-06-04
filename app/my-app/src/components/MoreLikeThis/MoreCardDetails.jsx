523ab240e7ab8f3c1eaf76d0e2ffd4cdaa201769
unknown <renzcaio@gmail.com>
2025-06-04 20:34:24 +0800

    MoreLikeThis Styling Adjustment

--- app/my-app/src/components/MoreLikeThis/MoreCardDetails.jsx
+++ app/my-app/src/components/MoreLikeThis/MoreCardDetails.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ticket from "../../assets/images/ticket-icon.svg";
 export const MoreCardDetails = ({
   title,
   city,
@@ -8,6 +9,7 @@ export const MoreCardDetails = ({
   price2,
   admission1,
   admission2,
+  type,
 }) => {
   const titleStyle = {
     // counter-rotate the text to keep it straight
@@ -18,39 +20,38 @@ export const MoreCardDetails = ({
 
     margin: 0,
     marginLeft: "22px",
-    marginTop: "25px",
+    marginTop: "1rem",
     textAlign: "left",
   };
 
   const cityStyle = {
     // counter-rotate the text to keep it straight
 
-    color: "white",
+    color: "rgba(250,250,250,0.6)",
     fontSize: "14px",
     wordWrap: "break-word",
 
     margin: 0,
     marginLeft: "22px",
-    marginTop: "27px",
+    marginTop: "4px",
     textAlign: "left",
   };
 
   const comStyle = {
     // counter-rotate the text to keep it straight
 
-    color: "white",
+    color: "rgba(250,250,250,0.6)",
     fontSize: "12px",
     wordWrap: "break-word",
 
     lineHeight: "1.3",
     margin: 0,
-    marginTop: "30px",
+    marginTop: "12px",
     marginLeft: "22px",
-    textAlign: "justify",
+    textAlign: "left",
     overflowWrap: "break-word",
     width: "80%",
     paddingBottom: "20px",
-    borderBottom: "dashed 1px white",
   };
 
   const burgerStyle = {
@@ -67,13 +68,15 @@ export const MoreCardDetails = ({
   const adsStyle = {
     // counter-rotate the text to keep it straight
 
-    color: "white",
+    color: "rgba(250,250,250,0.6)",
     fontSize: "12px",
     wordWrap: "break-word",
-
+    display: "flex",
+    alignItems: "center",
+    gap: "8px",
     margin: 0,
     marginLeft: "79px",
-    marginTop: "15px",
+    marginTop: "19px",
     textAlign: "left",
   };
 
@@ -84,7 +87,7 @@ export const MoreCardDetails = ({
     color: "white",
     fontSize: "18px",
     marginLeft: "79px",
-    marginTop: "21px",
+    marginTop: "18px",
     textAlign: "left",
   };
 
@@ -103,10 +106,10 @@ export const MoreCardDetails = ({
     // counter-rotate the text to keep it straight
 
     margin: 0,
-    color: "white",
+    color: "rgba(250,250,250,0.6)",
     fontSize: "11px",
     marginLeft: "79px",
-    marginTop: "25px",
+    marginTop: "2px",
     textAlign: "left",
   };
 
@@ -114,24 +117,43 @@ export const MoreCardDetails = ({
     // counter-rotate the text to keep it straight
 
     margin: 0,
-    color: "white",
+    color: "rgba(250,250,250,0.6)",
     fontSize: "11px",
     marginLeft: "179px",
     marginTop: "-15px",
     textAlign: "left",
   };
 
+  const dividerStyle = {
+    width: "100%",
+    border: "dashed 1px #0F0F0F",
+  };
+
+  const ticketStyle = {
+    width: "1rem",
+    height: "12px",
+  };
+
   return (
     <>
-      <h1 style={titleStyle}>{title}</h1>
+      <h1 style={titleStyle} className="more-title" title={title}>
+        {title}
+      </h1>
       <h1 style={cityStyle}>{city}</h1>
       <h3 style={comStyle}>{comment}</h3>
-
-      <h3 style={adsStyle}>&nbsp;&nbsp;{advertisement}</h3>
-      <h1 style={price1Style}>{price1}</h1>
-      <h1 style={price2Style}>{price2}</h1>
-      <h5 style={memStyle}>{admission1}</h5>
-      <h5 style={standStyle}>{admission2}</h5>
+      {type !== "known" && (
+        <>
+          <div style={dividerStyle}></div>
+          <h3 style={adsStyle}>
+            <img src={ticket} alt="ticket" style={ticketStyle} />
+            {advertisement}
+          </h3>
+          <h1 style={price1Style}>{price1}</h1>
+          <h1 style={price2Style}>{price2}</h1>
+          <h5 style={memStyle}>{admission1}</h5>
+          <h5 style={standStyle}>{admission2}</h5>
+        </>
+      )}
     </>
   );
 };
